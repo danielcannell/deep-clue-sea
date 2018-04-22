69413702c790441f3253c53a35cbbd57420d6acc
Aidan Woolley <aidan.woolley@cambridgeconsultants.com>
2018-04-22 22:52:40 +0100

    Add music to wins and losses

--- GameOver.gd
+++ GameOver.gd
@@ -1,4 +1,7 @@
 extends Container
 
-func _on_Button_pressed():
-    get_tree().change_scene("res://Main.tscn")
+func _ready():
+    connect("pressed", self, "loop")
+
+func loop():
+    play()
--- WinScreen.gd
+++ WinScreen.gd
@@ -2,6 +2,10 @@ extends Container
 
 func _ready():
     get_node("Solution").text = Globals.win_text
+    connect("finished", self, "loop")
 
 func _on_Button_pressed():
     get_tree().change_scene("res://Main.tscn")
+
+func loop():
+    play()
