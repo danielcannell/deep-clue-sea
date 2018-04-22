0bdd3de6c27d221f4a0600c6043203e95516f9b0
Aidan Woolley <aidan.woolley@cambridgeconsultants.com>
2018-04-22 17:32:38 +0100

    Implement sensible solution selection and knowledge distribution

--- detective/DetectiveRoot.gd
+++ detective/DetectiveRoot.gd
@@ -11,64 +11,56 @@ var RoomSolText = {
 }
 
 var solution = {"location":null, "traitor":null}
-
+var crew_knowledge = []
 
 func _ready():
-    # Get array of crew names, not allowing empty lines
-    var crew_name_file = File.new()
-    crew_name_file.open("res://crewmen/crewnames.txt", crew_name_file.READ)
-    var crew_name_pool = crew_name_file.get_as_text()
-    crew_name_pool = crew_name_pool.split("\n", false)
-    crew_name_file.close()
-    
-    # Necessary later for assigning knowledge around
-    var not_solution_name_list = []
-
-    # Assign each crew member a random name
-    for crew_id in range(Globals.NUM_CREW):
-        var crewman = get_node("root/Main/Submarine/CrewController/Crewman%s" % crew_id)
-        var name_ind = randi() % crew_name_pool.size()
-        crewman.crew_name = crew_name_pool[name_ind]
-        not_solution_name_list.append(crew_name_pool[name_ind])
-        crew_name_pool.remove(name_ind)
-    
-    # Pick a random crewman to be traitor
-    var traitor = get_node("root/Main/Submarine/CrewController/Crewman%s" % (randi() % Globals.NUM_CREW))
-    solution["traitor"] = traitor
-    solution["traitor"].traitor = true
-    not_solution_name_list.erase(traitor.crew_name)
-    
+    var num_crew = get_node("/root/Main/Submarine/CrewmenController").crewmen.size()
+    for i in range(num_crew):
+        crew_knowledge.append({"room_knowledge": [], "people_knowledge": []})
     # Randomly select a room to be the correct solution
-    solution["location"] = Globals.Rooms[randi() % 8]
-    var not_solution_rooms_list = Globals.Rooms.duplicate(true).erase(solution["location"])
+    solution["location"] = Globals.ROOMS_LIST[randi() % 8]
+    var not_solution_rooms_list = Globals.ROOMS_LIST.duplicate()
+    not_solution_rooms_list.erase(solution["location"])
+    
+    # Randomly select a traitor from list
+    solution["traitor"] = randi() % num_crew
+    
+    var not_solution_crew_list = []
+    
+    for i in range(num_crew):
+        not_solution_crew_list.append(i)
+    
+    not_solution_crew_list.erase(solution["traitor"])
+    
 
     # Assign knowledge around
     # Generate correct amount of knowledge
-    var not_guilty_crew = not_solution_name_list.length()
+    
+    var not_guilty_crew = not_solution_crew_list.size()
     for i in range(not_guilty_crew):
         for j in range(Globals.TRAITOR_KNOWLEDGE_COUNT - 1):
-            not_solution_name_list.append(not_solution_name_list[i])
+            not_solution_crew_list.append(not_solution_crew_list[i])
     
-    var not_solution_rooms = not_solution_rooms_list.length()
+    var not_solution_rooms = not_solution_rooms_list.size()
     for i in range(not_solution_rooms):
         for j in range(Globals.ROOM_KNOWLEDGE_COUNT - 1):
             not_solution_rooms_list.append(not_solution_rooms_list[i])
     
-    not_solution_rooms = not_solution_rooms_list.length()
-    not_guilty_crew = not_solution_name_list.length()
+    not_solution_rooms = not_solution_rooms_list.size()
+    not_guilty_crew = not_solution_crew_list.size()
     var i = 0
     for room_knl in range(not_solution_rooms):
-        var crew = get_node("root/Main/Submarine/CrewController/Crewman%s" % (i % Globals.NUM_CREW))
-        var knl_item = randi() % not_solution_rooms_list.length()
-        crew.room_knowledge.append(not_solution_rooms[knl_item])
+        var crew = i % num_crew
+        var knl_item = randi() % not_solution_rooms_list.size()
+        crew_knowledge[crew]["room_knowledge"].append(not_solution_rooms_list[knl_item])
         not_solution_rooms_list.remove(knl_item)
         i += 1
     
     for traitor_knl in range(not_guilty_crew):
-        var crew = get_node("root/Main/Submarine/CrewController/Crewman%s" % (i % Globals.NUM_CREW))
-        var knl_item = randi() % not_solution_name_list.length()
-        crew.room_knowledge.append(not_guilty_crew[knl_item])
-        not_solution_name_list.remove(knl_item)
+        var crew = i % num_crew
+        var knl_item = randi() % not_solution_crew_list.size()
+        crew_knowledge[crew]["people_knowledge"].append(not_solution_crew_list[knl_item])
+        not_solution_crew_list.remove(knl_item)
         i += 1
 
     
